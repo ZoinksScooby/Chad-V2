34cfc4e5b70dc6f942301a171febbc5cc81a8bc7
Robert Blinn <robertblinn@gmail.com>
2012-06-28 16:43:19 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChadV2::Application.config.secret_token = '8f5cfeb846b55153cb5af876f9cb4c0a8185ab1b2c94c6a15a70644bb1e990774dfd396126f17b7f468e4651a038d372861ec9d75a097fa95dd2565818594e8f'
